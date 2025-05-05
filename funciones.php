de88860c76ed174f66d349e8ac664122fd26bca5
Daniel Quintuña <daniel13464@MacBook-Pro-de-Daniel.local>
2025-05-05 17:13:24 -0500

    Funciones PHP

---
+++ funciones.php
@@ -0,0 +1,26 @@
+<?php
+//funcion sin parametros
+function saludo (){
+    echo"Buenas tardes. la fecha de hoy es: ";
+    echo date('Y-m-d')."</h2>";
+}
+
+//Funcion con envio de parametros
+function saludar ($nombre,$apellido){
+    echo "<h2>"."Buenas tardes, ".$nombre." ".$apellido."</h2>";
+}
+function multiplicar ($a,$b,$c){
+    echo "<h2>"."El resultado es: ".$a*$b*$c."<?h2>";
+}
+function areaCirculo($radio){
+    return pow($lado,2);
+}
+echo"<h1>"."Funciones en PHP"."</h1><br>";
+echo saludo();
+echo saludar("Diana", "Torres");
+echo saludar("Juan", "Perez");
+echo multiplicar(2,3,4);
+echo multiplicar(5,3,4);
+$r = 5;
+echo areaCirculo($r);
+?>
--- inicio.php
+++ inicio.php
@@ -3,15 +3,15 @@
 echo "<h1>"."Aprendiendo PHP"."</h1><BR>";
 $nombre = "Diana";
 $edad = 10;
-echo "Mi nombre es: ".$nombre." y mi edad es: ".$edad." anos";
+echo "Mi nombre es: ".$nombre." y mi edad es: ".$edad." años"."<br>";
 
 if ($edad >= 18)
-echo "Mayor de edad";
+echo "Mayor de edad"."<br>";
 else
-echo "Menor de edad";
+echo "Menor de edad"."<br>";
 
 $a = 1;
-while($a <5)
+while($a <= 5)
 {
     echo $a."<br>";
     $a++;
